a83f2dfab59640896821c4f8835e75c9f2063dc6
Agent-Sesame <a.l.julian@gmail.com>
2018-06-15 14:17:15 -0700

    code improvements
    Updated changewd.R to use relative paths.

--- changewd.R
+++ changewd.R
@@ -1,11 +1,31 @@
-# this code optimized for the way I store and manage git repos.
-# needs tweaking 
+# this function would need reworked in the context of the project in which
+# it's used.
 
 changewd <- function(x) {
-  old.dir <- getwd()
-  new.dir <- paste(setwd(".."), x, sep = "/")
-  setwd(new.dir)
-  print(getwd())
-  setwd(old.dir)
-  print(getwd())
+  
+  # list directories in current working directory
+  
+  check_dirs <- list.files()
+  
+  # does argument x exist current working directory?
+  
+  if (x %in% check_dirs) {
+    
+    # if yes, save old working directory, set new and print it, and restor old
+    
+    old.dir <- getwd()
+    setwd(paste(setwd("."), x, sep = "/"))
+    
+    # at this point, integrate the code into a different project
+    
+    print(getwd())
+    setwd(old.dir)
+    
+  } else {
+   
+    # if x does not exist do this 
+    
+    print(paste("Directory ", x, " does not exist", sep = "/"))
+    
+  }
 }
